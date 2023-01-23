73c68210ea7509cd2a14f8f0a6bbe2438c53a7cf
Giorgi <stichy.dl@gmail.com>
2023-01-23 21:48:58 +0400

    show loading page while fetching data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,43 @@
 import '../styles/globals.css'
+import Router from 'next/router'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../hooks/Auth'
 import { StoreProvider } from '../hooks/Store'
+import { useEffect, useState } from 'react'
+import Loading from '../components/Loading'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    const start = () => {
+      console.log('start')
+      setLoading(true)
+    }
+    const end = () => {
+      console.log('finished')
+      setLoading(false)
+    }
+    Router.events.on('routeChangeStart', start)
+    Router.events.on('routeChangeComplete', end)
+    Router.events.on('routeChangeError', end)
+    return () => {
+      Router.events.off('routeChangeStart', start)
+      Router.events.off('routeChangeComplete', end)
+      Router.events.off('routeChangeError', end)
+    }
+  }, [])
   return (
-    <StoreProvider>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </StoreProvider>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <StoreProvider>
+          <AuthProvider>
+            <Component {...pageProps} />
+          </AuthProvider>
+        </StoreProvider>
+      )}
+    </>
   )
 }
 
