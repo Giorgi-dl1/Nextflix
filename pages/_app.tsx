a4b299574ec4bb750745a7207c39f8fbcce59b91
Giorgi <stichy.dl@gmail.com>
2023-01-24 03:06:47 +0400

    update page loading handler

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,43 +1,33 @@
 import '../styles/globals.css'
 import Router from 'next/router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../hooks/Auth'
 import { StoreProvider } from '../hooks/Store'
-import { useEffect, useState } from 'react'
-import Loading from '../components/Loading'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(false)
   useEffect(() => {
-    const start = () => {
-      console.log('start')
-      setLoading(true)
-    }
-    const end = () => {
-      console.log('finished')
-      setLoading(false)
-    }
-    Router.events.on('routeChangeStart', start)
-    Router.events.on('routeChangeComplete', end)
-    Router.events.on('routeChangeError', end)
+    const handleRouteStart = () => NProgress.start()
+    const handleRouteDone = () => NProgress.done()
+
+    Router.events.on('routeChangeStart', handleRouteStart)
+    Router.events.on('routeChangeComplete', handleRouteDone)
+    Router.events.on('routeChangeError', handleRouteDone)
+
     return () => {
-      Router.events.off('routeChangeStart', start)
-      Router.events.off('routeChangeComplete', end)
-      Router.events.off('routeChangeError', end)
+      Router.events.off('routeChangeStart', handleRouteStart)
+      Router.events.off('routeChangeComplete', handleRouteDone)
+      Router.events.off('routeChangeError', handleRouteDone)
     }
   }, [])
   return (
-    <>
-      {loading ? (
-        <Loading />
-      ) : (
-        <StoreProvider>
-          <AuthProvider>
-            <Component {...pageProps} />
-          </AuthProvider>
-        </StoreProvider>
-      )}
-    </>
+    <StoreProvider>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </StoreProvider>
   )
 }
 
